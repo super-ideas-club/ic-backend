8243a65be431b107329922ad0484b05b6a5827ed
trashiodev <firemonstr2002@mail.ru>
2022-11-06 21:54:13 +0300

    admin panel fix for idea

--- ideasLogic/admin.py
+++ ideasLogic/admin.py
@@ -6,3 +6,10 @@ from ideasLogic.models import IdeaTheme, Idea, Team
 admin.site.register(Idea)
 admin.site.register(IdeaTheme)
 admin.site.register(Team)
+
+
+@admin.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+    fields = ['short_name', 'description', 'state', 'wanted_skills', 'themes', 'hidden']
+    list_filter = ['themes']
+
